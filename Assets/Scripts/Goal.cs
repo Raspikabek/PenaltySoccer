5c17eac5d0f372e025cdf3c31a60fd893bdb3882
Raspikabek <asenjo.rodriguez.eduardo@gmail.com>
2015-06-22 13:49:08 +0200

    First Approach. Added Colliders & GOAL message. Ball controlled with WASD

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Ball") {
+			Debug.Log ("GOAL!");
+		}
+	}
+}
